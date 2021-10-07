6ff9dbaff562e2d5a8fcf794ac8ed8e9700d860b
alfredoPark-48 <parkalfredojeonghyun@gmail.com>
2021-10-07 13:51:34 -0500

    Implemented selection sort

--- sorting_algorithms/selectionsort/main.cpp
+++ sorting_algorithms/selectionsort/main.cpp
@@ -3,6 +3,40 @@ using namespace std;
 
 int main()
 {
-  printf("Hello World!"); 
+  int arr[10] = {32, 64, 21, 24, 18, 9, 3, 90, 231, 100};
+  int size = sizeof(arr)/sizeof(arr[0]);
+  
+  // Printing before sorting
+  printf("Before sorting: ");
+  for (int i=0; i<size; i++)
+  {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+  
+  // Selection sort
+  for (int i=0; i<size-1; i++)
+  {
+    int lowestIndex = i;
+    for (int j=i+1; j<size; j++)
+    {
+      if (arr[lowestIndex] > arr[j])
+      {
+        lowestIndex = j;
+      }
+    }
+    int temp = arr[i];
+    arr[i] = arr[lowestIndex];
+    arr[lowestIndex] = temp;
+  }
+
+  // Printing after sorting
+  printf("After selection sort: ");
+  for (int i=0; i<size; i++)
+  {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+
   return 0;
 }
